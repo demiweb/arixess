fdfbdabc22d423a3ad653e73709992a7a931093e
JukaChu <olexiy.yankevich@gmail.com>
2023-10-12 18:13:23 +0300

    company

--- app/js/common.js
+++ app/js/common.js
@@ -1,93 +1,119 @@
-var $blogSteps = $('.big-article-cont');
-[...document.querySelectorAll('.steps-men')].forEach((btn, k) => {
-    let tt = btn.getBoundingClientRect().top - document.querySelector('.article-steps__cont').getBoundingClientRect().top;
-    // console.log(tt + '...' + k);
-    // console.log('height ' + btn.offsetHeight);
-});
-
-if ($blogSteps.length > 0 && !mobile) {
-    $blogSteps.find('.steps-men').css('left', $blogSteps.find('.big-article__text').offset().left);
-    $('.steps-men a').on('click', function (e) {
-        e.preventDefault();
-        var target = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(target).offset().top - $('header').height() - 50
-        }, 500);
-        return false;
-    });
-    $(window).on('scroll', function () {
-        var st = $(this).scrollTop();
-        var offset = $blogSteps.offset().top - $('header').height() - 300
-
-
-        var fromTop = $(this).scrollTop(),
-            menuItems = $('.steps-men a'),
-            scrollItems = menuItems.map(function () {
-                var item = $($(this).attr('href'));
-                if (item.length) return item;
-            }),
-            cur = scrollItems.map(function () {
-                if ($(this).offset().top - $('header').height() - 300 < fromTop) return this;
-            });
-        cur = cur[cur.length - 1];
-        var id = cur && cur.length ? cur[0].id : '';
-        menuItems.removeClass('active').filter('[href="#' + id + '"]').addClass('active');
-        console.log(menuItems.filter('[href="#' + id + '"]'));
-        if (menuItems.filter('[href="#' + id + '"]')[0]) {
-            let angl = 90;
-            let ps = parseInt(id.replace(/[^0-9]/gi, ''), 10);
-            // console.log(ps);
-            // console.log(menuItems.filter('[href="#' + id + '"]')[0]);
-            let hh = menuItems.filter('[href="#' + id + '"]')[0].getBoundingClientRect().top - document.querySelector('.article-steps__cont').getBoundingClientRect().top - (menuItems.filter('[href="#' + id + '"]')[0].offsetHeight / 4);
-            document.querySelector('.article-steps__cont').style.setProperty('--vr', `${hh}px`);
-            document.querySelector('.article-steps__cont').style.setProperty('--ang', `${angl * ps}deg`);
-            document.querySelector('.article-steps__cont').style.setProperty('--op', `1`);
-        } else {
-            let angl = 90;
-            document.querySelector('.article-steps__cont').style.setProperty('--ang', `90deg`);
-            document.querySelector('.article-steps__cont').style.setProperty('--op', `0`);
-        }
-
-    });
-}
-
-
-
-
 $(document).ready(function() {
-    var $slider = $('.results-slider');
-    var $progressBar = $('.progress-res');
+    var $slider2 = $('.ari-comm-slider');
+    var $progressBar1 = $('.ari-comments .progress-res');
 
-    var currT = 1;
-    let lgt = [...document.querySelectorAll('.results-slider .slide')].length;
+    var currT1 = 1;
+    let lgt1 = [...document.querySelectorAll('.ari-comm-slider .slide')].length;
 
 
 
-    $slider.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide) {
+    $slider2.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide) {
         let calc2 = (100 / slick.slideCount) * (currentSlide + 1);
 
-        $progressBar
+        $progressBar1
             .css('background-size', calc2 + '% 100%')
             .attr('aria-valuenow', calc2 );
 
 
         // $progressBarLabel.text( calc + '% completed' );
     });
-    $progressBar
-        .css('background-size', (100 / lgt) * (currT) + '% 100%')
-        .attr('aria-valuenow', (100 / lgt) * (currT) );
+    $progressBar1
+        .css('background-size', (100 / lgt1) * (currT1) + '% 100%')
+        .attr('aria-valuenow', (100 / lgt1) * (currT1) );
 
-    $slider.slick({
-        slidesToShow: 1,
+    $slider2.slick({
+        slidesToShow: 2,
         slidesToScroll: 1,
         speed: 400,
         infinite: false,
-        dots: false,
-        fade: true,
+        dots: true,
+        fade: false,
+        arrows: true,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
 
     });
-    var sldCr = $slider.slick;
-    console.log(sldCr);
+    var sldCr1 = $slider2.slick;
+    // console.log(sldCr1);
 
 });
+
+let tabBtn = [...document.querySelectorAll(".build-tabs__head .single-btn")];
+function changeTab() {
+    tabBtn.length && tabBtn.forEach((e,o)=>{
+            e.addEventListener("click", ()=>{
+                    e.classList.contains("active") || (tabBtn.forEach(e=>{
+                            e.classList.remove("active")
+                        }
+                    ),
+                        e.classList.add("active"),
+                        [...document.querySelectorAll(".build-tabs__wrap .single-build-tab")].forEach((e,t)=>{
+                                t === o ? e.classList.add("active") : e.classList.remove("active")
+                            }
+                        ))
+                }
+            )
+        }
+    )
+}
+changeTab();
+
+
+//modal window
+
+let btnMod = [...document.querySelectorAll('.btn-mod')];
+let modals = [...document.querySelectorAll('.modal-win')];
+
+
+function controlModal() {
+    if (btnMod.length) {
+        console.log('asdsdasdasd');
+
+        $('body').on('click', '.btn-mod', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            let data = this.dataset.mod;
+            console.log('asdqwdqwdqwd');
+            console.log(data);
+            modals.forEach((mod) => {
+                if (mod.dataset.modal === data) {
+                    document.body.classList.add('no-scroll');
+
+                    mod.classList.add('visible');
+
+                }
+            })
+        });
+
+
+        $('body').on('click', '.close', function (e) {
+
+                this.closest('.modal-win').classList.remove('visible');
+                document.body.classList.remove('no-scroll');
+        });
+
+
+        $('body').on('click', '.backplate', function (e) {
+            this.closest('.modal-win').classList.remove('visible');
+            document.body.classList.remove('no-scroll');
+
+        });
+
+
+
+    }
+}
+
+controlModal();
+
+
+
+
+
 
